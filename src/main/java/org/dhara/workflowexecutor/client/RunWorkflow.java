2199ee17ebe27273c3dcfd7fdaaec79d476ee826
harsha89 <harsz89@gmail.com>
2013-06-19 23:45:44 +0530

    initialize

---
+++ src/main/java/org/dhara/workflowexecutor/client/RunWorkflow.java
@@ -0,0 +1,145 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+package org.dhara.workflowexecutor.client;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.airavata.client.AiravataAPIFactory;
+import org.apache.airavata.client.api.AiravataAPI;
+import org.apache.airavata.client.api.exception.AiravataAPIInvocationException;
+import org.apache.airavata.registry.api.PasswordCallback;
+import org.apache.airavata.registry.api.exception.worker.ExperimentLazyLoadedException;
+import org.apache.airavata.registry.api.impl.WorkflowExecutionDataImpl;
+import org.apache.airavata.registry.api.workflow.ExperimentData;
+import org.apache.airavata.registry.api.workflow.InputData;
+import org.apache.airavata.registry.api.workflow.NodeExecutionData;
+import org.apache.airavata.registry.api.workflow.OutputData;
+import org.apache.airavata.rest.client.PasswordCallbackImpl;
+import org.apache.airavata.workflow.model.wf.InvalidDataFormatException;
+import org.apache.airavata.workflow.model.wf.Workflow;
+import org.apache.airavata.workflow.model.wf.WorkflowInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RunWorkflow {
+    private static final Logger log = LoggerFactory.getLogger(RunWorkflow.class);
+
+    private static int port;
+    private static String serverUrl;
+    private static String serverContextName;
+
+    private static String registryURL;
+
+    private static String gatewayName = "default";
+    private static String userName = "admin";
+    private static String password = "admin";
+
+    private static AiravataAPI airavataAPI;
+
+    public static void main(String[] args) throws Exception {
+
+        // creating airavata client object //
+        port = Integer.parseInt("8080");
+        serverUrl = "localhost";
+        serverContextName = "airavata-registry";
+        System.out.println((new File(".")).getAbsolutePath());
+        log.info("Configurations - port : " + port);
+        log.info("Configurations - serverUrl : " + serverUrl);
+        log.info("Configurations - serverContext : " + serverContextName);
+
+        registryURL = "http://" + serverUrl + ":" + port + "/" + serverContextName + "/api";
+
+        log.info("Configurations - Registry URL : " + registryURL);
+
+        PasswordCallback passwordCallback = new PasswordCallbackImpl(getUserName(), getPassword());
+        airavataAPI = AiravataAPIFactory.getAPI(new URI(getRegistryURL()), getGatewayName(), getUserName(),
+                passwordCallback);
+
+        String workflowName = "TestWorkflow";
+        Workflow workflow = airavataAPI.getWorkflowManager().getWorkflow(workflowName);
+        List<WorkflowInput> workflowInputs = workflow.getWorkflowInputs();
+        int count = 10;
+        for (WorkflowInput workflowInput : workflowInputs) {
+            if ("int".equals(workflowInput.getType())||"integer".equals(workflowInput.getType())) {
+                workflowInput.setValue(count--);
+            }
+        }
+        String result = airavataAPI.getExecutionManager().runExperiment(workflowName, workflowInputs);
+
+        System.out.println("Experiment ID Returned : " + result);
+        MonitorWorkflow.monitor(result);
+        airavataAPI.getExecutionManager().waitForExperimentTermination(result);
+        ExperimentData experimentData = airavataAPI.getProvenanceManager().getExperimentData(result);
+        List<WorkflowExecutionDataImpl> workflowInstanceData = experimentData.getWorkflowExecutionDataList();
+        System.out.println("Experiment Results");
+        System.out.println("==================");
+        for (WorkflowExecutionDataImpl executionDataImpl : workflowInstanceData) {
+			System.out.println("    Instnace ID :" +executionDataImpl.getId()+" ["+executionDataImpl.getTemplateName()+"]");
+			List<NodeExecutionData> nodeDataList = executionDataImpl.getNodeDataList();
+			for (NodeExecutionData nodeExecutionData : nodeDataList) {
+				System.out.println("        Node id :"+nodeExecutionData.getId()+"["+nodeExecutionData.getType().toString()+"]");
+				List<InputData> inputData = nodeExecutionData.getInputData();
+				for (InputData data : inputData) {
+					System.out.println("            [input] "+data.getName()+"="+data.getValue());
+				}
+				List<OutputData> outputData = nodeExecutionData.getOutputData();
+				for (OutputData data : outputData) {
+					System.out.println("            [output] "+data.getName()+"="+data.getValue());
+				}
+			}
+		}
+    }
+
+    public static String getRegistryURL() {
+        return registryURL;
+    }
+
+    public static String getGatewayName() {
+        return gatewayName;
+    }
+
+    public static String getUserName() {
+        return userName;
+    }
+
+    public static String getPassword() {
+        return password;
+    }
+
+    protected static String getWorkflowComposeContent() throws IOException {
+        System.out.println((new File(".")).getAbsolutePath());
+        BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/EchoWorkflow.xwf"));
+        String line = null;
+        StringBuffer buffer = new StringBuffer();
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        return buffer.toString();
+    }
+}
